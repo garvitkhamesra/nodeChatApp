9f0a75ffa4c256a4c9862f8ac3351ac6498ff3d6
Garvit Khamesra <garvitkhamesra@gmail.com>
2017-10-11 16:31:23 +0530

    Automatic scroller

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();    
+
+    if(newMessageHeight+lastMessageHeight+clientHeight+scrollTop >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect',function(){
     console.log("connected to server");
 });
@@ -16,18 +31,11 @@ socket.on('newMessage',function(message){
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');    
-    // var li = jQuery('<li></li>');
-    // var a = jQuery('<a target = "_blank">My Current location</a>')
-    // li.text(`${message.from}::${formattedTime} `);
-    // a.attr('href',message.url);
-    // li.append(a);
-    // jQuery('#messages').append(li);
-
     var template = jQuery('#location-message-template').html();
     var html = Mustache.render(template,{
         from:message.from,
@@ -35,6 +43,7 @@ socket.on('newLocationMessage',function(message){
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 var messageTextbox = jQuery('[name=message]');
